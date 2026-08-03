a1171b8fa1ae42b7b18c67ab53157a3db27adda2
Tahn Rainbird <14843@holbertonstudents.com>
2026-08-03 23:15:51 +0000

    fixed

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -8,12 +8,12 @@
  */
 int _strlen(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i] != '\0')
-                i++;
+	while (str[i] != '\0')
+		i++;
 
-        return (i);
+	return (i);
 }
 
 /**
