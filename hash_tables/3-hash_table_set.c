2b87c52857c46fad388918bb0f36d6ba86a0f31c
Tahn Rainbird <14843@holbertonstudents.com>
2026-08-12 06:48:47 +0000

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,42 +11,46 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = hash_djb2((const unsigned char *)key) % ht->size;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-	int i = 0, key_present = 1;
+	hash_node_t *new_node;
+	hash_node_t *curr_node;
 
+	if (ht == NULL)
+		return (0);
+
+	curr_node = ht->array[index];
+
+	while (curr_node != NULL)
+	{
+		if (strcmp(curr_node->key, key) == 0)
+		{
+			free(curr_node->value);
+			curr_node->value = strdup(value);
+			if (curr_node->value == NULL)
+				return (0);
+
+			return (1);
+		}
+		curr_node = curr_node->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	if (ht == NULL)
+	new_node->key = strdup(key);
+	if (new_node->key = NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	if (ht->array[index] == NULL)
+	if (new_node->value == NULL)
 	{
-		new_node->next = NULL;
-		ht->array[index] = new_node;
-		return (1);
+		free(new_node->key);
+		free(new_node);
+		return (0);
 	}
-	while (key[i] != '\0')
-	{
-		if (key[i] != ht->array[index]->key[i])
-		{
-			key_present = 0;
-			break;
-		}
-		i++;
-	}
-	if (key_present == 1)
-	{
-		free(ht->array[index]);
-		new_node->next = NULL;
-		ht->array[index] = new_node;
-		return (1);
-	}
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
 	return (1);
-}
