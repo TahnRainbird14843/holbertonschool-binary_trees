6d04a0c9c862164b9eca80abfcc935ee21921f1d
Tahn Rainbird <14843@holbertonstudents.com>
2026-08-12 00:55:24 +0000

    fixes

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,28 +15,34 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head,
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (head == NULL || new_node == NULL)
 		return (NULL);
 
 	if (idx == 0)
 		new_node = add_dnodeint(head, n);
 
-	while (curr_node->next != NULL && idx != i)
-		curr_node = curr_node->next, i++;
-
-	if (i != idx)
-		return (NULL);
-
-	if (curr_node->next == NULL && idx != 0)
-		new_node = add_dnodeint_end(head, n);
-
-	else if (idx != 0)
+	else
 	{
-		new_node->n = n;
-		new_node->prev = curr_node->prev;
-		new_node->next = curr_node;
-		(curr_node->prev)->next = new_node;
-		curr_node->prev = new_node;
+		while (curr_node->next != NULL && i != idx)
+		{
+			curr_node = curr_node->next;
+			i++;
+		}
+
+		if (i != idx)
+			return (NULL);
+
+		else if (curr_node->next == NULL)
+			new_node = add_dnodeint_end(head, n);
+
+		else
+		{
+			new_node->n = n;
+			new_node->prev = curr_node->prev;
+			new_node->next = curr_node;
+			(curr_node->prev)->next = new_node;
+			curr_node->prev = new_node;
+		}
 	}
 
 	return (new_node);
