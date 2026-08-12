66def8a3fe400fa86eb662af88d53a79979a0580
Tahn Rainbird <14843@holbertonstudents.com>
2026-08-12 06:31:39 +0000

    fixed

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,6 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = hash_djb2((const unsigned char *)key) % ht->size;
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	int i = 0;
+	int key_present = 1;
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
@@ -20,12 +22,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		new_node->next = NULL;
 		ht->array[index] = new_node;
+		return (1);
 	}
-	else
+	while (key[i] != '\0')
 	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
+		if (key[i] != ht->array[index]->key[i])
+		{
+			key_present = 0;
+			break;
+		}
 	}
+
+	if (key_present == 1)
+	{
+		free(ht->array[index]);
+		new_node->next = NULL;
+		ht->array[index] = new_node;
+		return (1);
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
