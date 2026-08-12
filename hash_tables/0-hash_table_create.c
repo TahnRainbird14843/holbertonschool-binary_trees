503168e92fc1a423aecc688bbef761c50d1f8263
Tahn Rainbird <14843@holbertonstudents.com>
2026-08-12 05:58:41 +0000

    fix

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = malloc(sizeof(hash_table));
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 	hash_node_t **array;
 
 	if (hash_table == NULL)
