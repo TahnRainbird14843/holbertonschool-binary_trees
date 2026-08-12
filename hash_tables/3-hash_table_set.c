ca0d07ffffa8a12af683168bb11550ffd60474e4
Tahn Rainbird <14843@holbertonstudents.com>
2026-08-12 06:50:07 +0000

    fixes

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -37,7 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
-	if (new_node->key = NULL)
+	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
@@ -54,3 +54,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[index] = new_node;
 
 	return (1);
+}
